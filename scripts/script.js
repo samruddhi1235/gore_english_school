a6baf2c4ada70e82dfdfc24b651af3b4b3c6fb4d
samruddhi1235 <samruddhi1235@gmail.com>
2026-08-23 13:39:23 +0530

    Delete scripts directory

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const navToggle = document.querySelector('.nav-toggle');
-  const navMenu = document.querySelector('nav ul');
-
-  navToggle.addEventListener('click', function() {
-    navMenu.classList.toggle('active');
-  });
-});
